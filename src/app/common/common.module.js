d0cd9f142b3d66cacd94f012ced12d705112b810
Jonathan Foster <jxf9099@rit.edu>
2018-02-04 15:12:42 -0500

    move home, update angularjs-scripts for style linting

--- src/app/common/common.module.js
+++ src/app/common/common.module.js
@@ -1,7 +1,9 @@
 import angular from 'angular';
+import HomeModule from './home/home.module';
 
 const CommonModule = angular
   .module('app.common', [
+    HomeModule
   ])
   .name;
 
--- src/app/components/components.module.js
+++ src/app/components/components.module.js
@@ -1,9 +1,7 @@
 import angular from 'angular';
-import HomeModule from './home/home.module';
 
 const ComponentsModule = angular
   .module('app.components', [
-    HomeModule
   ])
   .name;
 
